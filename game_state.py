556cca8e8585d4ae4654a5d4c06120663bb1ca54
campauhay <156327733+campauhay@users.noreply.github.com>
2024-09-27 16:20:40 -0230

    game state 1 working

--- game_state.py
+++ game_state.py
@@ -1,6 +1,6 @@
 import json
 #the fuckin problem - circular import
-from world import level1
+
 
 def handle_battle(attacks):
     result = battle(attacks)
@@ -11,23 +11,22 @@ def handle_battle(attacks):
         print("Battle ended.")
 
 def next_state(character_file):
+    from world import level1
     # Load the player's current state
     with open(character_file, 'r+') as save_file:
         save_data = json.load(save_file)
         
         save_data['state_number'] += 1
 
+        #increment game state
         current_state = save_data['state_number']
     #test print remove later
     print(f"You're now moving from state {current_state} to the next part of the game.")
-
-    # Increment the player's state
-    save_data['state_number'] = current_state + 1
 
     # Save the updated state back to the save file
     with open(character_file, 'w') as save_file:
         json.dump(save_data, save_file)
 
     # Load the new scene based on the updated state
-    level1()
+    level1(character_file)
 
